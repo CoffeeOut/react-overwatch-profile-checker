2d9c57d75002ac5e52d06329c6e3d65b6d55a2e0
CoffeeOut <coffee_out@bk.ru>
2020-07-13 01:23:28 +0400

    Added docs folder

---
+++ docs/precache-manifest.743d4909bb388ba97ce8759dff66c486.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd55ef722b711c946a176187f10d2c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89979213df5788085293",
+    "url": "/static/css/main.ce68a024.chunk.css"
+  },
+  {
+    "revision": "e578e9d27b0d74e8bea1",
+    "url": "/static/js/2.144ba151.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.144ba151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89979213df5788085293",
+    "url": "/static/js/main.bb01aa1f.chunk.js"
+  },
+  {
+    "revision": "e10d87f610cb26341723",
+    "url": "/static/js/runtime-main.11675dd9.js"
+  },
+  {
+    "revision": "1c2641dbf0a357aef713d4ee25a3ce7c",
+    "url": "/static/media/19439-webfont.1c2641db.ttf"
+  },
+  {
+    "revision": "67d3f3dac6011a2448bdf8009d46d19a",
+    "url": "/static/media/19439-webfont.67d3f3da.eot"
+  },
+  {
+    "revision": "da2d914c860bef53a1009175429a068d",
+    "url": "/static/media/19439-webfont.da2d914c.woff"
+  },
+  {
+    "revision": "f35ff37100f2a3fe07f5ed400cabbd38",
+    "url": "/static/media/19439-webfont.f35ff371.woff2"
+  },
+  {
+    "revision": "226cf5791cc5982d718806f6b241b53e",
+    "url": "/static/media/bignoodletoo-webfont.226cf579.woff2"
+  },
+  {
+    "revision": "c583b9c2136ad964b7f45496f48fac07",
+    "url": "/static/media/bignoodletoo-webfont.c583b9c2.woff"
+  },
+  {
+    "revision": "ceb1765e8ad67925e7cd0f587943f765",
+    "url": "/static/media/bignoodletoo-webfont.ceb1765e.ttf"
+  },
+  {
+    "revision": "ee77e6f527f5f51e87bde64dcc3f00f0",
+    "url": "/static/media/bignoodletoo-webfont.ee77e6f5.eot"
+  },
+  {
+    "revision": "6fec9a5e43cd44b7d00a710756795039",
+    "url": "/static/media/futurano2d_w03_regular-webfont.6fec9a5e.woff2"
+  },
+  {
+    "revision": "76e7f3dbfed569e859d583811d8e77e6",
+    "url": "/static/media/futurano2d_w03_regular-webfont.76e7f3db.woff"
+  },
+  {
+    "revision": "aa99abbf841f0f3c4dde919f5a8db01b",
+    "url": "/static/media/futurano2d_w03_regular-webfont.aa99abbf.eot"
+  },
+  {
+    "revision": "b4633486385ae9ca7d15c4c9d43dc55b",
+    "url": "/static/media/futurano2d_w03_regular-webfont.b4633486.ttf"
+  }
+]);
